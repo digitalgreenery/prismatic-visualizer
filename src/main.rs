6607ba38f48c3a8c3cb653c72e8ad5442050f5ad
digitalgreenery <digitalgreenery@proton.me>
2023-12-18 22:19:01 -0700

    Funny deformation

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod ui;
 use bevy::{
     prelude::{*},
     render::render_resource::{Extent3d, TextureDimension, TextureFormat, PrimitiveTopology},
-    render::mesh::Indices
+    render::mesh::Indices,
 };
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
@@ -19,7 +19,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(EguiPlugin)
         .add_systems(Startup, (setup))
-        .add_systems(Update, (rotate,ui_overlay))
+        .add_systems(Update, (rotate,ui_overlay,update_visualization))
         .run();
 }
 
@@ -30,7 +30,7 @@ struct Shape;
 #[derive(Component)]
 struct SphericalVisualizationMeshes;
 
-#[derive(Resource)]
+#[derive(Resource, Clone)]
 struct DefinedColorResource{
     component_limit: SColor,
     gamma: Gamma,
@@ -39,6 +39,7 @@ struct DefinedColorResource{
 }
 
 const X_EXTENT: f32 = 14.5;
+const SCALE: f32 = 5.0;
 
 fn setup(
     mut commands: Commands,
@@ -46,11 +47,6 @@ fn setup(
     mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let scale = 5.0;
-
-    let defined_color = DefinedColorResource{ component_limit: SColor::new(1., 1., 1.), gamma: Gamma::new(1., 1., 1.), hue_adjust: Gamma::new(1., 1., 1.), visualization_needs_updated: false };
-
-    commands.insert_resource(defined_color);
 
     let debug_material = materials.add(StandardMaterial {
         base_color_texture: Some(images.add(uv_debug_texture())),
@@ -95,35 +91,25 @@ fn setup(
 
     // ground plane
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
+        mesh: meshes.add(shape::Plane::from_size(100.0).into()),
         material: materials.add(Color::SILVER.into()),
         transform: Transform::from_xyz(0.,0.,-0.1).with_rotation( Quat::from_rotation_x(PI/2.)),
         ..default()
     });
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(scale*2., scale*2., scale*2.).looking_at(Vec3::new(0., 0., 0.), Vec3::Z),
+        transform: Transform::from_xyz(SCALE*2., SCALE*2., SCALE*2.).looking_at(Vec3::new(0., 0., 0.), Vec3::Z),
         ..default()
     });
 
-    let spherical_rgb_meshes = draw_spherical_colorspace();
-    for(_index, mesh) in spherical_rgb_meshes.iter().enumerate() {
-        commands.spawn((
-            PbrBundle {
-                mesh: meshes.add(mesh.clone()),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::WHITE,
-                    unlit: true,
-                    cull_mode: None,
-                    emissive: Color::rgb(1.0, 1.0, 1.0),// Set emissive color
-                    ..Default::default()
-                }),
-                transform: Transform::from_scale(Vec3 { x: scale, y: scale, z: scale }),
-                ..Default::default()
-            },
-            SphericalVisualizationMeshes,
-        ));
+    let defined_color = DefinedColorResource{ component_limit: SColor::new(1., 1., 1.), gamma: Gamma::new(1., 1., 1.), hue_adjust: Gamma::new(1., 1., 1.), visualization_needs_updated: false };
+    let defined_color_copy = DefinedColorResource{ component_limit: SColor::new(1., 1., 1.), gamma: Gamma::new(1., 1., 1.), hue_adjust: Gamma::new(1., 1., 1.), visualization_needs_updated: false };
 
-    }
+    commands.insert_resource(defined_color);
+
+    spawn_spherical_visualization(commands, meshes, materials, defined_color_copy);
+    
+    
+    
 
     // commands.insert_resource(ToggleCameraRotation(false));
  
@@ -257,9 +243,65 @@ fn ui_overlay(mut contexts: EguiContexts, mut defined_color: ResMut<DefinedColor
         //Update mesh
         //todo
         println!("Reached");
+        defined_color.visualization_needs_updated = true;
 
     }
 }
+
+fn update_visualization(
+    mut commands: Commands,
+    mut defined_color: ResMut<DefinedColorResource>,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+    entities: Query<Entity, With<SphericalVisualizationMeshes>>)
+{
+    if defined_color.visualization_needs_updated {
+        defined_color.visualization_needs_updated = false;
+
+        //Delete previous visualization 
+        for mesh in entities.iter(){
+            commands.entity(mesh).despawn();
+        }
+    
+    let defined_color_copy = DefinedColorResource{ 
+        component_limit: SColor::from_tuple(defined_color.component_limit.to_tuple()), 
+        gamma: Gamma::from_tuple(defined_color.gamma.to_tuple()), 
+        hue_adjust: Gamma::from_tuple(defined_color.hue_adjust.to_tuple()), 
+        visualization_needs_updated: false };
+
+        spawn_spherical_visualization(commands, meshes, materials, defined_color_copy);
+    }
+}
+
+fn spawn_spherical_visualization(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    defined_color: DefinedColorResource)
+{
+
+    let spherical_rgb_meshes = draw_spherical_colorspace(defined_color);
+    for(_index, mesh) in spherical_rgb_meshes.iter().enumerate() {
+        commands.spawn((
+            PbrBundle {
+                mesh: meshes.add(mesh.clone()),
+                material: materials.add(StandardMaterial {
+                    base_color: Color::WHITE,
+                    unlit: true,
+                    cull_mode: None,
+                    emissive: Color::rgb(1.0, 1.0, 1.0),// Set emissive color
+                    ..Default::default()
+                }),
+                transform: Transform::from_scale(Vec3 { x: SCALE, y: SCALE, z: SCALE }),
+                ..Default::default()
+            },
+            SphericalVisualizationMeshes,
+        ));
+
+    }
+
+}
+
 
 fn create_quad(v0: Vec3, v1: Vec3, v2: Vec3, v3: Vec3) -> Mesh {
     // Create a new mesh using a triangle list topology, where each set of 3 vertices composes a triangle.
@@ -303,29 +345,23 @@ fn create_quad(v0: Vec3, v1: Vec3, v2: Vec3, v3: Vec3) -> Mesh {
         ])))
 }
 
-fn draw_spherical_colorspace() -> Vec<Mesh>{
+fn draw_spherical_colorspace(defined_color: DefinedColorResource) -> Vec<Mesh>{
     let mut colorspace_meshes  = Vec::<Mesh>::new();
 
-    let h_step = 24;
-    let s_step = 8;
-    let v_step = 8;
+    let gamma = defined_color.gamma.clone();
+
+    let (h_step,s_step,v_step) = (24, 8, 8);
 
     for v in 0..v_step {
         let s_step_adjusted = s_step/(v_step-v);
         for s in 0..s_step_adjusted {
-            for h in  0..h_step{ 
-                
-                let point0 = spherical_hcl(h as f32 / h_step as f32,1.-(s as f32 /(s_step/(v_step-v)) as f32),v as f32 / v_step as f32).to_tuple();
-                let point1 = spherical_hcl(((h+1) % h_step) as f32 / h_step as f32, 1.-(s as f32 /(s_step/(v_step-v)) as f32), v as f32 /v_step as f32).to_tuple();
-                let point2 = spherical_hcl(h as f32 / (h_step) as f32, 1.-((s+1) as f32 /(s_step/(v_step-v)) as f32), v as f32 /v_step as f32).to_tuple();
-                let point3 = spherical_hcl(((h+1) % h_step) as f32 / (h_step) as f32, 1.-((s+1) as f32 /(s_step/(v_step-v)) as f32), v as f32 /v_step as f32).to_tuple();
-                
+            for h in  0..h_step{   
 
                 colorspace_meshes.push(create_quad(
-                Vec3::new(point0.0,point0.1,point0.2),
-                Vec3::new(point1.0,point1.1,point1.2),
-                Vec3::new(point2.0,point2.1,point2.2),
-                Vec3::new(point3.0,point3.1,point3.2)
+                    Vec3::from(DefinedColor::new(spherical_hcl(h as f32 / h_step as f32,1.-(s as f32 /(s_step/(v_step-v)) as f32),v as f32 / v_step as f32), gamma).to_color().to_tuple()),
+                    Vec3::from(DefinedColor::new(spherical_hcl(((h+1) % h_step) as f32 / h_step as f32, 1.-(s as f32 /(s_step/(v_step-v)) as f32), v as f32 /v_step as f32), gamma).to_color().to_tuple()),
+                    Vec3::from(DefinedColor::new(spherical_hcl(h as f32 / (h_step) as f32, 1.-((s+1) as f32 /(s_step/(v_step-v)) as f32), v as f32 /v_step as f32), gamma).to_color().to_tuple()),
+                    Vec3::from(DefinedColor::new(spherical_hcl(((h+1) % h_step) as f32 / (h_step) as f32, 1.-((s+1) as f32 /(s_step/(v_step-v)) as f32), v as f32 /v_step as f32), gamma).to_color().to_tuple()),
                 ));
 
             }
