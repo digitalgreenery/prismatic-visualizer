43d0a9001fa840012f12283142cda01be905e6db
digitalgreenery <digitalgreenery@proton.me>
2023-12-20 19:41:31 -0700

    Setup Wasm and html entry point

---
+++ wasm/build_wasm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ..
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --no-typescript --target web \
+    --out-dir ./wasm/ \
+    --out-name "spherical_rgb_visualizer" \
+    ./target/wasm32-unknown-unknown/release/spherical_rgb_visualizer.wasm
