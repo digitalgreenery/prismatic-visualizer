b216fa4307a940edf9313059d0606acffe529943
digitalgreenery <digitalgreenery@proton.me>
2023-12-20 20:27:43 -0700

    Fixed luminance and chroma bounds

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@
 
 
 use spherical_rgb::{Color as SColor, DefinedColor, Gamma, spherical_hcl, cubic_hsv};
-use std::{f32::consts::PI};
+// use std::{f32::consts::PI};
 
 
 use bevy::{
     prelude::{*},
-    render::render_resource::{Extent3d, TextureDimension, TextureFormat, PrimitiveTopology},
+    render::render_resource::{/*Extent3d, TextureDimension, TextureFormat,*/ PrimitiveTopology},
     render::mesh::Indices,
 };
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
@@ -47,45 +47,45 @@ enum VisualiztionShape{
     Cubic,
 }
 
-const X_EXTENT: f32 = 14.5;
+// const X_EXTENT: f32 = 14.5;
 const SCALE: f32 = 5.0;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
+    // mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
 
-    let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(uv_debug_texture())),
-        ..default()
-    });
+    // let debug_material = materials.add(StandardMaterial {
+    //     base_color_texture: Some(images.add(uv_debug_texture())),
+    //     ..default()
+    // });
 
-    let shapes = [
-        meshes.add(shape::Cube::default().into()),
-        meshes.add(shape::Icosphere::default().try_into().unwrap()),
-        meshes.add(shape::UVSphere::default().into()),
-    ];
+    // let shapes = [
+    //     meshes.add(shape::Cube::default().into()),
+    //     meshes.add(shape::Icosphere::default().try_into().unwrap()),
+    //     meshes.add(shape::UVSphere::default().into()),
+    // ];
 
-    let num_shapes = shapes.len();
+    // let num_shapes = shapes.len();
 
-    for (i, shape) in shapes.into_iter().enumerate() {
-        commands.spawn((
-            PbrBundle {
-                mesh: shape,
-                material: debug_material.clone(),
-                transform: Transform::from_xyz(
-                    -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                    7.0,
-                    1.0,
-                )
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
-                ..default()
-            },
-            Shape,
-        ));
-    }
+    // for (i, shape) in shapes.into_iter().enumerate() {
+    //     commands.spawn((
+    //         PbrBundle {
+    //             mesh: shape,
+    //             material: debug_material.clone(),
+    //             transform: Transform::from_xyz(
+    //                 -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+    //                 7.0,
+    //                 1.0,
+    //             )
+    //             .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+    //             ..default()
+    //         },
+    //         Shape,
+    //     ));
+    // }
 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -341,6 +341,14 @@ fn spawn_spherical_visualization(
 
 }
 
+fn get_four_points(f: fn(f32,f32,f32)-> SColor, gamma: Gamma, hue: f32, hue_next: f32, chroma: f32, chroma_next: f32, luminance: f32, luminance_next: f32)-> (Vec3,Vec3,Vec3,Vec3){
+    (
+        Vec3::from(gamma.apply_gamma(&f(hue,chroma, luminance)).to_tuple()),
+        Vec3::from(gamma.apply_gamma(&f(hue_next, chroma, luminance)).to_tuple()),
+        Vec3::from(gamma.apply_gamma(&f(hue, chroma_next, luminance_next)).to_tuple()),
+        Vec3::from(gamma.apply_gamma(&f(hue_next, chroma_next, luminance_next)).to_tuple()),
+    )
+}
 
 fn create_quad(v0: Vec3, v1: Vec3, v2: Vec3, v3: Vec3, settings: VisualizationSettings) -> Mesh {
     
@@ -417,9 +425,8 @@ fn draw_spherical_colorspace(settings: VisualizationSettings) -> Vec<Mesh>{
             cubic_hsv,
         };
 
-    for v in 0..v_step {
-        // let s_step_adjusted = (s_step as f32*(v as f32 /v_step as f32)) as u8;
-        for s in 0..s_step/*_adjusted*/ {
+    for v in 0..=v_step {
+        for s in 0..=s_step{
             for h in  0..h_step{   
 
                 // let (red, green, blue) = spherical_hcl(h as f32 / h_step as f32,1.-(s as f32 /(s_step/(v_step-v)) as f32),v as f32 / v_step as f32).to_tuple();
@@ -462,50 +469,40 @@ fn draw_spherical_colorspace(settings: VisualizationSettings) -> Vec<Mesh>{
 }
 
 
+// Creates a colorful test pattern
+// fn uv_debug_texture() -> Image {
+//     const TEXTURE_SIZE: usize = 12;
 
-fn get_four_points(f: fn(f32,f32,f32)-> SColor, gamma: Gamma, hue: f32, hue_next: f32, chroma: f32, chroma_next: f32, luminance: f32, luminance_next: f32)-> (Vec3,Vec3,Vec3,Vec3){
-    (
-        Vec3::from(gamma.apply_gamma(&f(hue,chroma, luminance)).to_tuple()),
-        Vec3::from(gamma.apply_gamma(&f(hue_next, chroma, luminance)).to_tuple()),
-        Vec3::from(gamma.apply_gamma(&f(hue, chroma_next, luminance_next)).to_tuple()),
-        Vec3::from(gamma.apply_gamma(&f(hue_next, chroma_next, luminance_next)).to_tuple()),
-    )
-}
+//     let mut palette = [0; TEXTURE_SIZE*4];
 
-/// Creates a colorful test pattern
-fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 12;
-
-    let mut palette = [0; TEXTURE_SIZE*4];
-
-    for n in 0..TEXTURE_SIZE{
-        let float_color = DefinedColor::new(spherical_hcl((n)as f32/(TEXTURE_SIZE)as f32, 1.0, 1.0),Gamma::new(2.2,2.2,2.2,)).to_color().to_tuple();
-        palette[n*4]    =(float_color.0*255.0)as u8;
-        palette[n*4+1]  =(float_color.1*255.0)as u8;
-        palette[n*4+2]  =(float_color.2*255.0)as u8;
-        palette[n*4+3]  =255;
-        println!("Color {n} is: {float_color:?}");
-    }
+//     for n in 0..TEXTURE_SIZE{
+//         let float_color = DefinedColor::new(spherical_hcl((n)as f32/(TEXTURE_SIZE)as f32, 1.0, 1.0),Gamma::new(2.2,2.2,2.2,)).to_color().to_tuple();
+//         palette[n*4]    =(float_color.0*255.0)as u8;
+//         palette[n*4+1]  =(float_color.1*255.0)as u8;
+//         palette[n*4+2]  =(float_color.2*255.0)as u8;
+//         palette[n*4+3]  =255;
+//         println!("Color {n} is: {float_color:?}");
+//     }
 
 
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
+//     let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+//     for y in 0..TEXTURE_SIZE {
+//         let offset = TEXTURE_SIZE * y * 4;
+//         texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
+//         palette.rotate_right(4);
+//     }
 
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-    )
-}
+//     Image::new_fill(
+//         Extent3d {
+//             width: TEXTURE_SIZE as u32,
+//             height: TEXTURE_SIZE as u32,
+//             depth_or_array_layers: 1,
+//         },
+//         TextureDimension::D2,
+//         &texture_data,
+//         TextureFormat::Rgba8UnormSrgb,
+//     )
+// }
 
 
 // struct ToggleCameraRotation(bool);
