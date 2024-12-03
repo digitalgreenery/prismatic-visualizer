26480110dab66ed036e6a2ed26967aa46afaf6e7
digitalgreenery <148732116+digitalgreenery@users.noreply.github.com>
2024-12-03 16:31:35 -0700

    Delete wasm directory

--- wasm/build_wasm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --no-typescript --target web \
-    --out-dir ./wasm/ \
-    --out-name "spherical_rgb_visualizer" \
-    ./target/wasm32-unknown-unknown/release/spherical_rgb_visualizer.wasm
