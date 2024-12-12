ded9e2299c8914d162661b446a1e3908a97d0259
digitalgreenery <digitalgreenery@protonmail.com>
2024-12-12 06:47:22 -0700

    messed up by pushing to color

---
+++ wasm/wasm-build-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export RUSTFLAGS=--cfg=web_sys_unstable_apis
+cargo clean
+cargo build --release --target wasm32-unknown-unknown || exit 1
+wasm-bindgen --no-typescript --target web \
+    --out-dir ./wasm/ \
+    --out-name "prismatic_visualizer" \
+    ./target/wasm32-unknown-unknown/release/prismatic_visualizer.wasm || exit 1
+
+wasm-opt -Oz -o wasm/prismatic_visualizer_bg.wasm wasm/prismatic_visualizer_bg.wasm || exit 1
+
+echo "Build and optimization completed successfully."
